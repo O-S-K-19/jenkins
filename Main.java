cb0c41bca9afc4d4695e5ef78c887edde2e7e627
Ousseini <96442377+O-S-K-19@users.noreply.github.com>
2023-01-31 21:59:42 +0100

    Update Main.java
    test

--- Main.java
+++ Main.java
@@ -1,1 +1,1 @@
-public class Main{public static void main(String[] args){System.out.println("hello");}} 
+public class Main{public static void main(String[] args){System.out.println("hello ousse");}} 
